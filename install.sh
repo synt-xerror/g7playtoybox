589210673e59e4148a36056c5bad7a7e0f9a6041
Syntax <toningamer36023@gmail.com>
2025-10-31 23:48:07 -0300

    teste 2

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Iniciando instalação do SE-G7PlayToyBox..."
 sleep 2
-git clone https://github.com/synt-xerror/g7playtoybox.git
+git clone https://github.com/synt-xerror/g7playtoybox
 sudo mv g7playtoybox/src/g7ptoybox.sh /bin/g7ptoybox
 mv g7playtoybox/src/* $HOME/.config/g7ptoybox/
 rm -r g7playtoybox
