a3f8b5e8e3ce7a8ad500df8e10bb51adebfef775
Syntax <toningamer36023@gmail.com>
2025-10-31 23:52:59 -0300

    teste 3

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 echo "Iniciando instalação do SE-G7PlayToyBox..."
 sleep 2
 git clone https://github.com/synt-xerror/g7playtoybox
+sudo rm -f /bin/g7ptoybox
 sudo mv g7playtoybox/src/g7ptoybox.sh /bin/g7ptoybox
+mkdir -p $HOME/.config/g7ptoybox/
 mv g7playtoybox/src/* $HOME/.config/g7ptoybox/
-rm -r g7playtoybox
+rm -rf g7playtoybox
 
 echo "Instalação concluída. Execute no terminal: 'g7ptoybox'"
