0d737ed907db90c02c3e73fea27bcc688e92ab9f
Syntax <toningamer36023@gmail.com>
2025-11-01 01:19:41 -0300

    style: version at menu
    v0.3.0-alpha.2

--- src/g7ptoybox.sh
+++ src/g7ptoybox.sh
@@ -60,7 +60,7 @@ function logo() {
     sleep 0.05
     echo -e "     ${DARK_GREEN}--------  --   ----   -      ------   --  "
     sleep 0.05
-    echo -e "               ${DARK_GREEN}--   -      ----- --    --  --    v0.3.1-alpha.1"
+    echo -e "               ${DARK_GREEN}--   -      ----- --    --  --    v0.3.0-alpha.2"
     sleep 0.1
     echo -e "              ${DARK_GREEN}--      ${GREEN}__                ___.                 "
     sleep 0.05
